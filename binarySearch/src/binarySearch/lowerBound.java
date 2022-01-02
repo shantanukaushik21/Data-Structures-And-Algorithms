80b52639eb569b1637bd07455b55bf6163fac622
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2022-01-02 16:46:19 +0530

    lower Bound

--- binarySearch/src/binarySearch/lowerBound.java
+++ binarySearch/src/binarySearch/lowerBound.java
@@ -1,29 +1,28 @@
 package binarySearch;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class lowerBound {
 	public static void main(String[] args) {
-		int[] arr= {4,2,1,2,3,4,5,6};
+		int[] arr= {4,2,1,2,31,2,1,1,1,1,1,4,5,6};
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
-		System.out.println(LB(arr,2));
+		System.out.println(LB(arr,1));
 	}
 	public static int LB(int[] nums, int target) {
 		int start=0;
 		int end=nums.length-1;
-		int mid=(start+end)/2;
-		int ans=-1;
+		int ans=Integer.MAX_VALUE;
 		while(start<=end) {
+			int mid=(start+end)/2;
+			System.out.println("Vlue of mid = "+mid);
 			if(nums[mid]==target) {
-				ans=nums[mid];
-				break;
-			}
-			if(nums[mid]>target) {
+				ans=Math.min(ans, mid);
 				end=mid-1;
 			}
-			if(nums[mid]<target) {
+			else if(nums[mid]>target) {
+				end=mid-1;
+			}
+			else if(nums[mid]<target) {
 				start=mid+1;
 			}
 		}
