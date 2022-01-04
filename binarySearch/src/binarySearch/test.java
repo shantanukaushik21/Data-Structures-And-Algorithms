b21a1d95b442010cc5d422704a91d9e35aea5dbb
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2022-01-04 23:45:41 +0530

    1315 binary search

--- binarySearch/src/binarySearch/test.java
+++ binarySearch/src/binarySearch/test.java
@@ -4,8 +4,32 @@ import java.util.Arrays;
 
 public class test {
 	public static void main(String[] args) {
-		int[][] arr= {{6,5,1,-1,-2},{6,4,3,2,1},{9,7,6,5,3},{9,7,4,3,2}};
-		System.out.println(Arrays.toString(arr[3]));
+		int[] arr= {5,1,3,-1,-3,6};
+		
+//		Pair p=new Pair(3,4);
+//		System.out.println(p.first);
+		Pair[] pairArr=new Pair[arr.length];
+		for(int i=0;i<arr.length;i++) {
+			pairArr[i]=new Pair(arr[i],i);
+		}
+		
+//		for(int i=0;i<pairArr.length;i++) {
+//			System.out.println(pairArr[i].toString());
+//		}
+		//pair
+		
 	}
 
+}
+class Pair{
+	int first;
+	int second;
+	@Override
+	public String toString() {
+		return "Pair [first=" + first + ", second=" + second + "]";
+	}
+	Pair(int first,int second){
+		this.first=first;
+		this.second=second;
+	}
 }
