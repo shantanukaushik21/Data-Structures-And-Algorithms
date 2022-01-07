1f0089cdca723e7e3028106c6e97307633e2cf95
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2022-01-07 21:11:13 +0530

    349 Intersection of Two Arrays

---
+++ binarySearch/src/hashMap/IntersectionofTwoArrays.java
@@ -0,0 +1,33 @@
+package hashMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class IntersectionofTwoArrays {
+
+//	leetcode 349. Intersection of Two Arrays
+}
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashMap<Integer, Boolean> map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            map.put(nums1[i], false);
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums2.length;i++){
+            if(map.containsKey(nums2[i])){
+                if(!map.get(nums2[i])){
+                    list.add(nums2[i]);
+                    map.put(nums2[i],true);
+                }
+            }
+        }
+        int [] arr = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
+        }
+        return arr;
+    }
+}
