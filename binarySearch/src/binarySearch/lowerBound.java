506332632ebdfceb575458adf8030e5a1146de93
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2022-01-09 17:55:00 +0530

    1011. Capacity To Ship Packages Within D Days

--- binarySearch/src/binarySearch/lowerBound.java
+++ binarySearch/src/binarySearch/lowerBound.java
@@ -7,7 +7,7 @@ public class lowerBound {
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
 		System.out.println(LB(arr,2));
-		System.out.println(UB(arr,2));
+		System.out.println(UB(arr,7));
 	}
 	public static int LB(int[] nums, int target) {
 		int start=0;
@@ -30,7 +30,8 @@ public class lowerBound {
 			}
 		}
 		if(!flag) {
-			return -1;
+			//if the target value is not present then this will return the index of value just smaller than the Target value 
+			return start-1;
 		}
 		return ans;
 	}
@@ -39,16 +40,22 @@ public class lowerBound {
 		int start=0;
 		int end=nums.length-1;
 		int ans=-1;
+		boolean flag=false;
 		while(start<=end) {
 			int mid=(start+end)/2;
 			if(nums[mid]==target) {
 				ans=Math.max(ans, mid);
 				start=mid+1;
+				flag=true;
 			}else if(nums[mid]>target) {
 				end=mid-1;
 			}else if(nums[mid]<target) {
 				start=mid+1;
 			}
+		}
+		if(!flag) {
+			//if the target value is not present then this will return the index of value just larger than the Target value 
+			return end+1;
 		}
 		return ans;
 	}
