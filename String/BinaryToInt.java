ad17f6d0410928214fff93f36847d4a602d5358f
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2022-01-30 23:59:59 +0530

    binary to Int

--- String/BinaryToInt.java
+++ String/BinaryToInt.java
@@ -25,4 +25,19 @@ public class BinaryToInt {
 		}
 		return ans;
 	}
+	//number of steps to 0 using -1 if odd divide by 2 if even
+	
+	public static int process(int num) {
+		int n=num;
+		int ans=0;
+		while(n>=0) {
+			if(n%2==0) {
+				n=n/2;
+			}else {
+				n=n-1;
+			}
+			ans++;
+		}
+		return ans;
+	}
 }
