62f60af483eb55290ac9d1987b56f4eabf668eed
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2022-02-01 23:54:15 +0530

    wine Problem Memoization

--- DP/wineProblem.java
+++ DP/wineProblem.java
@@ -5,27 +5,34 @@ import java.util.Arrays;
 public class wineProblem {
 	public static void main(String[] args) {
 		int[] arr= {4,1,2,5,3};
-		
-		int[] dp=new int[arr.length+1];
-//		dp[0]=0;
-		
-		//////////
-		for(int i=0;i<arr.length+1;i++) {
-			dp[i]=-1;
+		int len=arr.length;
+		int[][] dp=new int[len+1][len+1];
+		for(int i=0;i<=arr.length;i++) {
+			for(int j=0;j<=arr.length;j++) {
+				dp[i][j]=-1;
+			}
 		}
-		System.out.println(maxProfit(arr,0, arr.length-1,1));
-		System.out.println(Arrays.toString(dp));
+//		for(int i=0;i<=arr.length;i++) {
+//			for(int j=0;j<=arr.length;j++) {
+//				System.out.print(dp[i][i]);
+//			}
+//			System.out.println();
+//		}
+		
+		System.out.println(maxProfit(arr,0, arr.length-1,1,dp));
+//		System.out.println(Arrays.toString(dp));
 	}
-	public static int maxProfit(int[] arr, int start, int end, int year) {
+	public static int maxProfit(int[] arr, int start, int end, int year, int[][] dp) {
 		if(start>end) {
 			return 0;
 		}
-		if(year>arr.length) {
-			return 0;
-		}
 
-		int first = arr[start]*year + maxProfit(arr, start+1,end,year+1);
-		int second = arr[end]*year + maxProfit(arr, start, end-1,year+1);
+		if(dp[start][end]!=-1) {
+			return dp[start][end];
+		}
+		
+		int first = arr[start]*year + maxProfit(arr, start+1,end,year+1,dp);
+		int second = arr[end]*year + maxProfit(arr, start, end-1,year+1,dp);
 		return Math.max(first,second);
 	}
 }
