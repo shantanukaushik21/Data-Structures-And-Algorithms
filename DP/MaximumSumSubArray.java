b124b928d7115b0c3118f52ec45769fdb2182e78
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2022-02-04 00:22:50 +0530

    Max Subarray Sum

--- DP/MaximumSumSubArray.java
+++ DP/MaximumSumSubArray.java
@@ -1,9 +1,14 @@
 package dp;
 
+import java.util.Arrays;
+
 public class MaximumSumSubArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[] arr = {3,1,2,-2,1,-1,-100,4};
+		System.out.println(MaxSum(arr));
 
 	}
 	public static int MaxSum(int[] arr) {
@@ -17,10 +22,15 @@ public class MaximumSumSubArray {
 		int maxSumSoFar=dp[0];
 		int sum=dp[0];
 		for(int i=1;i<arr.length;i++) {
+			dp[i]=dp[i-1]+arr[i];
+			if(dp[i]<0) {
+				dp[i]=0;
+			}
+			maxSumSoFar=Math.max(maxSumSoFar, dp[i]);
 			
 		}
-		
-		return dp[arr.length-1];
+		System.out.println(Arrays.toString(dp));
+		return maxSumSoFar;
 	}
 
 }
